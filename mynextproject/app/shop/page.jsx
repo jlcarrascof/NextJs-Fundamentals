ebcd30df957538cb7798029b90d305bb8885dbf3
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-23 17:19:15 -0400

    Update Shop page

--- mynextproject/app/shop/page.jsx
+++ mynextproject/app/shop/page.jsx
@@ -1,6 +1,14 @@
 function Shop() {
   return (
-    <div>Shop</div>
+    <div>
+        <h1>Shop</h1>
+
+        <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+        </ul>
+    </div>
   )
 }
 
