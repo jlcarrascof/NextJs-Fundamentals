abaf08948c7358fc9733db9eb175643c9a39b5c9
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-26 02:23:48 -0400

    Fix some formatting errors

--- mynextproject/app/shop/layout.jsx
+++ mynextproject/app/shop/layout.jsx
@@ -1,6 +1,14 @@
 export default function ShopLayout({ children }) {
-  return 
+  return ( 
     <>
+      <nav>
+        <h3>Section Shop</h3>
+        <ul>
+            <li>Categories</li>
+            <li>Products</li>
+        </ul>    
+      </nav>  
       {children}
     </>
+ );
 }
