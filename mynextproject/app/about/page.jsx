577a1bb14e01ded45778082163c74e58ceaa7dfb
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-27 18:51:50 -0400

    Using metadata in about section

--- mynextproject/app/about/page.jsx
+++ mynextproject/app/about/page.jsx
@@ -1,3 +1,9 @@
+export const metadata = { 
+  title: 'Acerca de mi tienda con Next.js',
+}
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return <div>
+    About
+  </div>;
 }   
