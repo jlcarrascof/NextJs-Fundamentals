fa2045e00edd61210093992a88e01f29d31ff3bb
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-27 18:39:55 -0400

    Using metadata with their values: title, description, keywords

--- mynextproject/app/layout.jsx
+++ mynextproject/app/layout.jsx
@@ -1,5 +1,12 @@
 import Navbar from '../components/Navbar'
 
+export const metadata = { 
+  title: 'Mi tienda con Next.js - HomePage',
+  description: 'Esta es la página de inicio de mi tienda con Next.js',
+  keywords: 'tienda, next.js, ecommerce',
+
+}
+
 export default function RootLayout({ children }) {
  return (
     <html>
