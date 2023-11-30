70dcadfe8d9d30f1af178f6491a9f94df72b385b
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-30 18:16:54 -0400

    Update Homepage content

--- mynextproject/app/page.jsx
+++ mynextproject/app/page.jsx
@@ -1,8 +1,10 @@
+"use client"
+
+import Users from "../components/Users.jsx"
+
 export default function HomePage() {
     return <section>
         <h1>Hello World</h1>
-        <button>
-            Click
-        </button>
+        <Users />
     </section>
 }
