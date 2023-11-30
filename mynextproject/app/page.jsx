cec6f12639d0d50d495993b9e15b3f2827551333
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-30 18:32:23 -0400

    Using a constant metadata to show how work React Server Components

--- mynextproject/app/page.jsx
+++ mynextproject/app/page.jsx
@@ -1,10 +1,14 @@
 import Users from "../components/Users.jsx"
 
+export const metadata = {
+    title: "My Special Home Page",
+} 
+
 export default function HomePage() {
     //* server component
     return <section>
         <h1>Hello World</h1>
-        //* client component
+        {/* client component */}
         <Users />
     </section>
 }
