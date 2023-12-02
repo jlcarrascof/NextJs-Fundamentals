3ad9de41d110fe96930d2eefc503123e65847d84
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-02 01:38:57 -0400

    Create the PostCard component

--- mynextproject/app/posts/page.jsx
+++ mynextproject/app/posts/page.jsx
@@ -15,6 +15,9 @@ async function PostPages() {
                     <div key={post.id}>
                         <h3>{post.id}.- {post.title}</h3>
                         <p>{post.body}</p>
+                        <button onClick={() => {}}>
+                            Click
+                        </button>
                     </div>
                 ))
             }
