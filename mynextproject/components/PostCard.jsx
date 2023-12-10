00f2cb61094cad24c3ddd62a61efcaf7d3ac46b5
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-10 13:45:15 -0400

    Link PostCard component with the appropiate route

--- mynextproject/components/PostCard.jsx
+++ mynextproject/components/PostCard.jsx
@@ -1,9 +1,12 @@
 'use client'
+import Link from 'next/link'
 
 function PostCard({ post }) {
     return (
         <div>
-            <h3>{post.id}.- {post.title}</h3>
+            <Link href={`/posts/${post.id}`}>
+                <h3>{post.id}.- {post.title}</h3>
+            </Link>
             <p>{post.body}</p>
             <button onClick={() => {
                 alert(`You clicked ${post.id}`)
