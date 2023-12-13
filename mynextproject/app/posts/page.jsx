64e73f9365886c96ca2a3b2a136d34fc24101cf1
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-12 22:41:38 -0400

    Activate the Promise commented to delay 5 seconds in loading

--- mynextproject/app/posts/page.jsx
+++ mynextproject/app/posts/page.jsx
@@ -4,7 +4,7 @@ async function loadPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await res.json()
 
-    // await new Promise((resolve) => setTimeout(resolve, 3000))    
+    await new Promise((resolve) => setTimeout(resolve, 5000))    
 
     return data
 }
