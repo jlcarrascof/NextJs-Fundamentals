72b939382b884f8454739516129dfc5ff74e3661
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-14 23:03:46 -0400

    Use import alias in posts-page (PostCard)

--- mynextproject/app/posts/page.jsx
+++ mynextproject/app/posts/page.jsx
@@ -1,4 +1,5 @@
-import PostCard from '../../components/PostCard'
+// import PostCard from '../../components/PostCard'
+import PostCard from '@/components/Post';
 
 async function loadPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
--- mynextproject/app/shop/categories/computers/page.jsx
+++ mynextproject/app/shop/categories/computers/page.jsx
@@ -1,5 +1,5 @@
 // import PostCard from '../../../../components/PostCard'
-import PostCard from '@/components/PostCard'
+// import PostCard from '@/components/PostCard'
 
 export const metadata = { 
   title: 'Computers section',
