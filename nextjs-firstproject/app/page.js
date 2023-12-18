4b56bdb38cc055a77040f9f729da42a3fc89f685
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-18 02:28:29 -0400

    Apply Tailwind styles to page and layout

--- nextjs-firstproject/app/page.js
+++ nextjs-firstproject/app/page.js
@@ -11,11 +11,11 @@ async function HomePage() {
   return (
     <ul>
       {users.map(user => (
-        <li key={user.id}>
+        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex">
           <div>
-            <h5>{user.id} {user.first_name} {user.last_name}</h5>
+            <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
           </div>
-          <p>Email: {user.email}</p>
+          <p className="text-slate-100">Email: {user.email}</p>
           <img src={user.avatar} alt={user.first_name} />
         </li>
       ))}
