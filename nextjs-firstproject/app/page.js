a0bbb27a65664efc3ae491bb3266f88cf6185c46
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-18 01:42:39 -0400

    Create and testing my function fetchUsers()

--- nextjs-firstproject/app/page.js
+++ nextjs-firstproject/app/page.js
@@ -1,4 +1,13 @@
-function HomePage() {
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users")
+  const data = await res.json()
+  return data
+}
+
+async function HomePage() {
+
+  const users = await fetchUsers()
+
   return (
     <div>HomePage</div>
   )
