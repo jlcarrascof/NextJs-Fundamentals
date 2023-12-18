68891488a4a559468bd1434711754967085dc36f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-18 11:26:49 -0400

    Finish basic styles

--- nextjs-firstproject/app/page.js
+++ nextjs-firstproject/app/page.js
@@ -11,12 +11,12 @@ async function HomePage() {
   return (
     <ul>
       {users.map(user => (
-        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex">
+        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between">
           <div>
             <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
+            <p className="text-slate-100">Email: {user.email}</p>
           </div>
-          <p className="text-slate-100">Email: {user.email}</p>
-          <img src={user.avatar} alt={user.first_name} />
+          <img src={user.avatar} alt={user.first_name} className="rounded-full w-20" />
         </li>
       ))}
     </ul>  
