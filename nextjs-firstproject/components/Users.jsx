d6a217f8015c24a657677fe2436742d20d4fff5f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-18 11:45:04 -0400

    Use onClick event in Users component

--- nextjs-firstproject/components/Users.jsx
+++ nextjs-firstproject/components/Users.jsx
@@ -4,7 +4,10 @@ function Users({ users }) {
   return (
     <ul>
         {users.map(user => (
-        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between">
+        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between"
+         onClick={()=> {
+            alert(`${user.id}`);
+         }} >
             <div>
             <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
             <p className="text-slate-100">Email: {user.email}</p>
