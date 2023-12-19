fe45e964878f75e86aea9ce797ba547750e8251f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 01:11:39 -0400

    Add a Link element

--- nextjs-firstproject/components/Users.jsx
+++ nextjs-firstproject/components/Users.jsx
@@ -1,19 +1,19 @@
 "use client";
+import Link from "next/link";
 
 function Users({ users }) {
   return (
     <ul>
         {users.map(user => (
-        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between"
-         onClick={()=> {
-            alert(`${user.id}`);
-         }} >
-            <div>
-            <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
-            <p className="text-slate-100">Email: {user.email}</p>
-            </div>
-            <img src={user.avatar} alt={user.first_name} className="rounded-full w-20" />
-        </li>
+        <Link href={`/users/${user.id}`} key={user.id}>  
+          <li className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between">
+              <div>
+              <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
+              <p className="text-slate-100">Email: {user.email}</p>
+              </div>
+              <img src={user.avatar} alt={user.first_name} className="rounded-full w-20" />
+          </li>
+        </Link>
         ))}
     </ul>
   )
