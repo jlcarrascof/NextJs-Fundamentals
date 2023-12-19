4fd91b1d8ba725ae928b1e07a89c053fdfa5b296
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 01:43:27 -0400

    Create async function getUser()

--- nextjs-firstproject/app/users/[id]/page.jsx
+++ nextjs-firstproject/app/users/[id]/page.jsx
@@ -1,3 +1,9 @@
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  return data.data
+}
+
 export default function UserPage({ params }) {
   console.log(params)  
   return (
