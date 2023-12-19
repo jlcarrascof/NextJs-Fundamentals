73e97202c74871415342b172076bf5d70bc2cac5
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 02:56:42 -0400

    Finish out first operative NextJS application

--- nextjs-firstproject/components/Navbar.jsx
+++ nextjs-firstproject/components/Navbar.jsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
 
 function Navbar() {
     return (
-        <nav>
+        <nav className='bg-slate-400 mb-4 flex justify-between 
+        items-center px-20 p-3 font-bold text-black'>
             <Link href="/">
                 Home
             </Link>
