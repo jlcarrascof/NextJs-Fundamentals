741c0a048705cb7a4f91883492f046396528e4cc
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-25 23:00:48 -0400

    Modifying about page

--- mynextproject/src/app/about/page.jsx
+++ mynextproject/src/app/about/page.jsx
@@ -3,7 +3,18 @@ export const metadata = {
 }
 
 export default function AboutPage() {
-  return <div>
-    About
-  </div>;
+  return <section>
+    <h1>About</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut a 
+      corporis recusandae velit rerum possimus inventore deserunt consectetur ea, 
+      quidem mollitia exercitationem! Repudiandae atque aliquam dolorum fuga 
+      doloremque autem. Quia deserunt possimus maiores alias aspernatur enim ipsum, 
+      quo sequi quae perspiciatis quis laboriosam quod, unde doloremque expedita 
+      sint! Obcaecati exercitationem, provident consequuntur vel odit, velit 
+      architecto vitae earum fugit maiores cum consequatur aperiam totam laborum 
+      nesciunt expedita. Neque qui in ea delectus nemo, a sapiente consequuntur 
+      totam dolores voluptatibus asperiores quod optio rerum pariatur vitae rem 
+      molestias labore saepe aperiam reiciendis provident. Magnam est ullam eos 
+      iste aut natus accusamus.</p>
+  </section>;
 }   
