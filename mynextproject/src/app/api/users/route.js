8482d7b9106520ab215d20b2f5384fc959c19e7d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-10 13:07:35 -0400

    Inside of route.js create the messages for GET, POST, PUT and DELETE

--- mynextproject/src/app/api/users/route.js
+++ mynextproject/src/app/api/users/route.js
@@ -1,3 +1,30 @@
+import { NextResponse } from 'next/server';
+
+// Extract params
+// Query Database    
+// Return response
+
 export function GET() {
-    return Response("Hello World");
+    return NextResponse.json({
+        message: 'Obteniendo Datos!',
+    });
 }    
+
+export function POST() {
+    return NextResponse.json({
+        message: 'Creando Datos!',
+    });
+}
+
+export function PUT() {
+    return NextResponse.json({
+        message: 'Actualizando Datos!',
+    });
+}    
+
+export function DELETE() {
+    return NextResponse.json({
+        message: 'Eliminando Datos!',
+    });
+}    
+
